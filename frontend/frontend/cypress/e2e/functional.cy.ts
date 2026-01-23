6c69ff09b7f18ec63820917ae268153801104ed5
Hamza KHRIBICH <hamza.khribich@gmail.com>
2026-01-23 13:59:22 +0100

    added cart functionnal test

--- frontend/frontend/cypress/e2e/functional.cy.ts
+++ frontend/frontend/cypress/e2e/functional.cy.ts
@@ -1,53 +1,94 @@
-import { loginUser } from "cypress/support/utils.cy";
-import * as userData from "../fixtures/user.json";
+import { loginUser } from 'cypress/support/utils.cy';
+import * as userData from '../fixtures/user.json';
 const user = userData;
 
 describe('Functional Tests', () => {
-  it('Connexion', () => loginUser(user))
+  it('Connexion', () => loginUser(user));
 
   it('Panier', () => {
     cy.intercept({
       method: 'PUT',
-      url: `${Cypress.env('API_URL')}/orders/add`
-    }).as('APICheckAddToCart')
+      url: `${Cypress.env('API_URL')}/orders/add`,
+    }).as('APICheckAddToCart');
 
-    loginUser(user)
-    cy.navigate('nav-link-products', '/products')
+    loginUser(user);
+    cy.navigate('nav-link-products', '/products');
 
     cy.get('.list-products > [data-cy="product"]')
       .first()
       .find('[data-cy="product-link"]')
-      .click()
+      .click();
 
-      // Extraction du stock dans la page de détail du produit
-    cy.getByData('detail-product-quantity').invoke('text').then((text) => {
-      const stock = Number(text.split(" ")[0])
+    // Extraction du stock dans la page de détail du produit
+    cy.getByData('detail-product-stock')
+      .wait(500)
+      .invoke('text')
+      .then((text) => {
+        const stock: number = Number(text.split(' ')[0]);
+
+        cy.wrap(stock).as('initialStock');
 
         // Extraction de la valeur actuelle de la quantité
-      cy.getByData('detail-product-quantity').invoke('val').then((val) => {
-        const quantityValue = Number(val)
+        cy.getByData('detail-product-quantity')
+          .invoke('val')
+          .then((val) => {
+            const quantityValue = Number(val);
+            const shouldBeDisabled =
+              stock < 1 || quantityValue < 1 || quantityValue > 20;
 
-        if(stock < 1 || quantityValue < 1 || quantityValue > 20) {
-          cy.getByData('detail-product-add').should('be.disabled')
-        } else {
-              // Elements de la nouvelle page
-            cy.getByData('detail-product-quantity').clear().type('2');
-            cy.getByData('detail-product-add').click();
+            cy.getByData('detail-product-add').then((btn) => {
+              const isDisabled = btn.is(':disabled');
 
-              // Vérification API et retour
-            cy.wait('@APICheckAddToCart').its('response.statusCode').should('eq', 200);
+              if (shouldBeDisabled) {
+                if (!isDisabled) {
+                  throw new Error(
+                    'Le bouton "Ajouter au panier" devrait être désactivé mais ne l\'est pas.',
+                  );
+                }
+              } else {
+                expect(isDisabled).to.be.false;
 
-            cy.url().should('include', '/#/cart');
-            cy.go('back');
+                cy.url().as('previousUrl');
 
-            // Vérification de la mise à jour du stock sur la page de détail
-            cy.getByData('detail-product-stock').should('contain', stock - 2);
-        }
-      })
-    })
-  })
-})
+                // Ajout au panier
+                cy.getByData('detail-product-quantity').clear().type('2');
+                cy.getByData('detail-product-add').click();
 
+                // Vérification API
+                cy.wait('@APICheckAddToCart')
+                  .its('response.statusCode')
+                  .should('eq', 200);
+                cy.url().should('include', '/cart');
+
+                // Forcer la conversion TypeScript
+                cy.get('@previousUrl').then((url) => {
+                  cy.visit(url as unknown as string);
+                });
+
+                cy.wait(500);
+
+                // Vérification du stock mis à jour
+                cy.getByData('detail-product-stock')
+                  .wait(500)
+                  .invoke('text')
+                  .then((text) => {
+                    cy.get('@initialStock').then((initialStock: any) => {
+                      const updatedStock = Number(text.split(' ')[0]);
+                      const expectedStock = initialStock - 2;
+
+                      cy.log(`Stock initial: ${initialStock}`);
+                      cy.log(`Stock attendu: ${expectedStock}`);
+                      cy.log(`Stock actuel: ${updatedStock}`);
+
+                      expect(updatedStock).to.eq(expectedStock);
+                    });
+                  });
+              }
+            });
+          });
+      });
+  });
+});
 
 /*
 product = cy.get('.list-products > [data-cy="product"]')
